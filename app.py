250bfe34448d6025f4b7f2e33d5ba733eb756821
mohitsawale123-bit <mohitsawale123@gmail.com>
2026-04-16 01:14:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -691,6 +691,9 @@ while True:
 
         candles_1m = get_klines("1m", 100)
         candles_5m = get_klines("5m", 100)
+        if MODE == "DATA":
+    update_csv(candles_1m, "btc_1m.csv")
+    update_csv(candles_5m, "btc_5m.csv")
 
         if not candles_1m or not candles_5m:
             print("⚠️ Candle fetch failed")
